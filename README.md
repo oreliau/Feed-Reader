c1278c6ad76fbbfde49d1cff0b62653ed0d2980a
KOUMEDZRO Oreliau <oreliauk@gmail.com>
2018-08-20 22:36:14 +0200

    review of 20.08.2018

--- README.md
+++ README.md
@@ -4,12 +4,23 @@
 
 * [Introduction](#introduction)
 * [How to run the application](#How to run the application)
+* [What did I test?](#What did I test?)
 
 ## Instruction
 
-This is my first test with jasmine, is very usefull.
+For this project I use **jasmine** api 2.1.2 version. It's a helpfull api whitch can give a test of your code create by own.
+[source](https://jasmine.github.io)
+So this project show us how jasmine work on a web page, i had some test to write by udacity and make them work.
+When all is good jasmine is green and if a test fail jasmine part become red and show your where you wrong.
 
 ## How to run the application
 
-open index.html
+open index.html and go at the end and the web page, you will see jasmaine api
 
+## What did I test?
+
+So we will ask to me 'what did you test?' Well udacity expeted to me that i could test:
+1. Does RSS Feeds defined (name and url)
+2. Does menu humberger works (start hide and if the click show or hide it)
+3. Does entry has more than 0 entries
+4. Does new feed load well
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,26 +4,26 @@
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
+/* I let all of the tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-
-    describe('RSS Feeds', function() {
-
+	//This the first part of the test and check if all of Feed are empty or not.
+	describe('RSS Feeds', function() {
+		//check if var all is defined
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
+		//Check if names of feeds are defined.
         it("name are defined", function(){
             for( i =0; i < allFeeds.length; i++){
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
             };
         });
-
+		//Check if url of feeds are defined
         it("URL are defined", function(){
             for( i =0; i < allFeeds.length; i++){
                 expect(allFeeds[i].url).toBeDefined();
@@ -31,13 +31,13 @@ $(function() {
             };
         });
     });
-
+	//Secondly we check the hamburger menu if it works good.
     describe('The menu', function(){
-
+		//Check if he start hide when we oppen the wweb page
         it('menu hidden', function(){
           expect($('body').hasClass('menu-hidden')).toEqual(true);
         });
-
+		//Check if it hide or show when we click on it.
         it('menu changes', function(){
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass("menu-hidden")).toBe(false);
@@ -47,31 +47,40 @@ $(function() {
     });
 
     describe('initial Entries', function(){
-
+		//This is for asynchronus function load the first feed before begin the test
         beforeEach(function (done) {
             loadFeed(0, function(){
                 done();
             });
         });
-
+		//Check if entry has more than 0 entries
         it('define if entry has more than 0 entries', function(){
-            expect($('.entry .Feed')).toBeDefined();
+            expect($('.feed')).not.toBeNull();
+			expect($('.entry')).not.toBeNull();
+			expect($('.entry')).toBeDefined();
         });
     });
-
+	//Check if it changes content when new feed is load
     describe('New Feed Selection', function(){
-
+		let prevUrl;
+		let newUrl;
+		//load first feed in prevUrl variable
         beforeEach(function(done){
-            $('.feed').empty();
             loadFeed(0, function () {
-                entriesStart = $('.feed').find(allFeeds.url);
+                prevUrl = $('.feed').find(allFeeds.url);
                 done();
             });
+			//load second feed in next variable.
             loadFeed(1, function () {
-                entriesStart = $('.feed').find(allFeeds.url);
+                newUrl = $('.feed').find(allFeeds.url);
+				
                 done();   
             });
         });
+		//And check that prevUrl and newUrl if they are diffrent
+		it('changes content when new feed is load', function(){
+				expect(prevUrl).not.toEqual(newUrl);
+				});
 
     });
 
